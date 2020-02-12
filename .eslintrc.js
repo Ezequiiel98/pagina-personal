c8bd04460d4dbcc4342f9555038945af9840d418
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-02-12 20:42:28 -0300

    Creo el proyecto react acomodo carpetas y archivos. Y  tambien instalo eslint, eslint-config-wolox-react y sass

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+  },
+  'extends': [
+    'plugin:react/recommended',
+    'eslint-config-wolox-react',
+  ],
+  'plugins': [
+    'react'
+  ]
+ };
