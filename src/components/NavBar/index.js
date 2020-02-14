6575efe8ba370bd9986ddb1496fcf7cdaf5ff805
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-02-14 01:10:43 -0300

    agrego funcion para mostrar y sacar el menu burguer

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -3,14 +3,31 @@ import React from 'react';
 import styles from './index.module.scss';
 
 function NavBar() {
+  let i = true;
+
   let handleClick = (event)=>{
     event.preventDefault()
-    document.querySelector('#nav').classList.add(styles.navBarActive)
+    if (i) {
+      document.querySelector('#nav').style = ' height: 100%; width: 100%';
+      i = !i
+    } else { 
+      document.querySelector('#nav').style = ' height: 0; width: 0';
+
+      i = !i
+    }
   }
+
+  let resizeW = () =>{
+    if(window.innerWidth >= 768){
+      document.querySelector('#nav').style = ' height: auto; width: auto';
+    }
+  }
+  window.onresize = resizeW
+
   return (
     <div className={styles.containerNav}>
       <div className={styles.containterBurger}>
-        <a href="google.com" className={styles.burgerMenu} onClick={handleClick}>
+        <a href="#" className={styles.burgerMenu} onClick={handleClick}>
           <span className={styles.burgerSpan}></span>
           <span className={styles.burgerSpan}></span>
           <span className={styles.burgerSpan}></span>
